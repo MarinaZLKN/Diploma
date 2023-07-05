7fa7c431208ae9d217b692b638b81124c8f72862
Marina <marina.zilkina@gmail.com>
2023-07-05 16:40:30 +0300

    models, selializers, views

--- silant/backend/models.py
+++ silant/backend/models.py
@@ -1,3 +1,158 @@
 from django.db import models
 
-# Create your models here.
+
+# Техническая модель
+class TechnicalModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Модель трансмиссии
+class TransmissionModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Модель двигетеля
+class EngineModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Модель управляемого моста
+class ControlledBridgeModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Модель ведущего моста
+class DrivingBridgeModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Тип технического обслуживания
+class TypeOfMaintenance(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Метод восстановления
+class RecoveryMethod(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Клиент
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class FailureNode(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Сервисная компания
+class ServiceCompany(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# Машина
+class Machine(models.Model):
+    machine_factory_number = models.CharField(max_length=4)
+    engine_factory_number = models.CharField(max_length=100)
+    transmission_factory_number = models.CharField(max_length=100)
+    driving_bridge_factory_number = models.CharField(max_length=100)  # Зав. номер ведущего моста
+    controlled_bridge_factory_number = models.CharField(max_length=100)  # Зав. номер управляемого моста
+    delivery_contract = models.CharField(max_length=100)  # Договор поставки номера, дата
+    shipment_date = models.DateField()  # дата отгрузки с завода
+    consignee = models.CharField(max_length=100)  # грузополучатель
+    delivery_address = models.CharField(max_length=100)
+    equipment = models.CharField(max_length=100)  # комплектация
+
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    service_company = models.ForeignKey(ServiceCompany, on_delete=models.CASCADE)
+    engine_model = models.ForeignKey(EngineModel, on_delete=models.CASCADE)
+    technical_model = models.ForeignKey(TechnicalModel, on_delete=models.CASCADE)
+    transmission_model = models.ForeignKey(TransmissionModel, on_delete=models.CASCADE)
+    driving_bridge_model = models.ForeignKey(DrivingBridgeModel, on_delete=models.CASCADE)
+    controlled_bridge_model = models.ForeignKey(ControlledBridgeModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.machine_factory_number
+
+
+# ТО
+class Maintenance(models.Model):
+    date_of_maintenance = models.DateField()  # дата ТО
+    operating_time = models.IntegerField()  # наработка, м/час
+    order_number = models.CharField(max_length=100)  # номер заказ-наряда
+    data_of_order = models.DateField()  # дата заказ-наряда
+
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)  # Организация, проводившая ТО
+    type_of_maintenance = models.ForeignKey(TypeOfMaintenance, on_delete=models.CASCADE)
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    service_company = models.ForeignKey(ServiceCompany, on_delete=models.CASCADE)
+
+
+# Рекламации
+class Claim(models.Model):
+    date_of_failure = models.DateField()  # дата отказа
+    operating_time = models.IntegerField()  # наработка, м/час
+    spare_parts_used = models.TextField(null=True, blank=True, default=None)  # используемые запасные части
+    date_of_recovery = models.DateField()  # дата восстановления
+    technical_downtime = models.IntegerField(default=None)  # время простоя техники
+    description_of_failure = models.CharField(max_length=100)  # Описание отказа
+
+    failure_node = models.ForeignKey(FailureNode, on_delete=models.CASCADE)  # узел отказа
+    recovery_method = models.ForeignKey(RecoveryMethod, on_delete=models.CASCADE)
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    service_company = models.ForeignKey(ServiceCompany, on_delete=models.CASCADE)
+
+    # по сути, дата восстановления - дата отказа
+    def downtime(self):
+        self.technical_downtime = (self.date_of_recovery - self.date_of_failure).days
+        print(self.technical_downtime)
+        self.save()
+
---
+++ silant/backend/serializers.py
@@ -0,0 +1,143 @@
+from .models import *
+from rest_framework import serializers
+
+
+class ServiceCompanySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ServiceCompany
+        fields = ['id', 'name', 'description', ]
+
+
+class TechnicalModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TechnicalModel
+        fields = ['id', 'name', 'description', ]
+
+
+class EngineModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = EngineModel
+        fields = ['id', 'name', 'description', ]
+
+
+class TransmissionModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TransmissionModel
+        fields = ['id', 'name', 'description', ]
+
+
+class ControlledBridgeModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ControlledBridgeModel
+        fields = ['id', 'name', 'description', ]
+
+
+class DrivingBridgeModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = DrivingBridgeModel
+        fields = ['id', 'name', 'description', ]
+
+
+class TypeOfMaintenanceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TypeOfMaintenance
+        fields = ['id', 'name', 'description', ]
+
+
+class RecoveryMethodSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = RecoveryMethod
+        fields = ['id', 'name', 'description', ]
+
+
+class ClientSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Client
+        fields = ['id', 'name', 'description', ]
+
+
+class OrganizationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Organization
+        fields = ['id', 'name', 'description', ]
+
+
+class FailureNodeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = FailureNode
+        fields = ['id', 'name', 'description', ]
+
+
+class MachineSerializer(serializers.HyperlinkedModelSerializer):
+    service_company = serializers.StringRelatedField()
+    technical_model = serializers.StringRelatedField()
+    engine_model = serializers.StringRelatedField()
+    transmission_model = serializers.StringRelatedField()
+    driving_bridge_model = serializers.StringRelatedField()
+    controlled_bridge_model = serializers.StringRelatedField()
+    client = serializers.StringRelatedField()
+
+    class Meta:
+        model = Machine
+        fields = ['id',
+                  'machine_factory_number',
+                  'engine_factory_number',
+                  'transmission_factory_number',
+                  'driving_bridge_factory_number',
+                  'controlled_bridge_factory_number',
+                  'delivery_contract',
+                  'shipment_date',
+                  'consignee',
+                  'delivery_address',
+                  'equipment',
+                  'client',
+                  'service_company',
+                  'technical_model',
+                  'engine_model',
+                  'transmission_model',
+                  'driving_bridge_model',
+                  'controlled_bridge_model',
+                  ]
+
+
+class MaintenanceSerializer(serializers.HyperlinkedModelSerializer):
+    type_of_maintenance = serializers.StringRelatedField()
+    organization = serializers.StringRelatedField()
+    machine = serializers.StringRelatedField()
+    service_company = serializers.StringRelatedField()
+
+    class Meta:
+        model = Maintenance
+        fields = ['id',
+                  'date_of_maintenance',
+                  'operating_time',
+                  'order_number',
+                  'data_of_order',
+                  'organization',
+                  'type_of_maintenance',
+                  'machine',
+                  'service_company',
+                  ]
+
+
+class ClaimSerializer(serializers.HyperlinkedModelSerializer):
+    recovery_method = serializers.StringRelatedField()
+    failure_node = serializers.StringRelatedField()
+    description_of_failure = serializers.StringRelatedField()
+    machine = serializers.StringRelatedField()
+    service_company = serializers.StringRelatedField()
+
+    class Meta:
+        model = Claim
+        fields = ['id',
+                  'date_of_failure',
+                  'operating_time',
+                  'failure_node',
+                  'spare_parts_used',
+                  'date_of_recovery',
+                  'technical_downtime',
+                  'recovery_method',
+                  'description_of_failure',
+                  'machine',
+                  'service_company',
+                  ]
